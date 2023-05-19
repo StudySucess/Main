3f8e12d84a948c2e508cd11577d5beabc92c365d
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2023-05-19 11:08:31 +0200

    Added rudimentary login/register system (NEEDS FIXING)

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+/* NOTICE: In order for this config file to work, you must first creat a MySQL database with the name "demo",
+as well as the following table:
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
