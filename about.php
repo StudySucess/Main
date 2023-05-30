1cc9dc74df8bbe276233c83f43ad3ef3728dffa6
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2023-05-30 10:30:27 +0200

    Added About page

---
+++ about.php
@@ -0,0 +1,34 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="./style/main.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+
+<body>
+<header class="header headerGradient">
+    <ul class="listItems">
+        <li><a href="index.php"><img src="images/SSHLogo.png" class="headerLogo"></a></li>
+        <li>
+            <a href="classes.php">Vakken</a>
+            <a href=""><img src="images/header/dropdownSel.svg"></a>
+        </li>
+        <!--<li class="searchElement">
+            <a href="search.html"><img></a>
+            <input type="text" class="searchBar" placeholder="Zoek studiemateriaal / posts">
+            <div class="searchSymbol"></div>
+        </li>-->
+        <li><a href="">Over ons</a></li>
+        <li>
+            <button onclick="window.location.href='logIn.php';" class="logInBtn">Log in</button>
+        </li>
+    </ul>
+</header>
+
+</body>
+
+</html>
