d41a5dca50581cf9ec0c678aa8c3c52f8426268d
Karim Altir <mhdkarim48850@gmail.com>
2023-05-31 10:00:02 +0200

    password root

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ CREATE TABLE users (
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_NAME', 'demo');
 
 /* Attempt to connect to MySQL database */
